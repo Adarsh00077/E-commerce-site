033a43ce056d4a967fdd14de283cb7a1d003b274
Jitendra Singh Rajpurohit <Ji10raj@gmail.com>
2021-05-29 02:13:13 +0530

    settings

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -121,6 +121,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 import os
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS=[
