07298f201f9fa4ab489eda592f9081ef6e019639
Jitendra Singh Rajpurohit <Ji10raj@gmail.com>
2021-05-29 16:48:53 +0530

    settings

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9(j)$i4#8h+vxwzv0ozws0vj&r112@%37*p^unb!$)u0*zv0(-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','shopnotion.herokuapp.com']
+ALLOWED_HOSTS = ['shopnotion.herokuapp.com']
 
 
 # Application definition
