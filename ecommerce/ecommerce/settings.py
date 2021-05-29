ebc5fb93459a7c99489bc4e86dc89c0c0240640e
Jitendra Singh Rajpurohit <Ji10raj@gmail.com>
2021-05-29 16:53:07 +0530

    .

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9(j)$i4#8h+vxwzv0ozws0vj&r112@%37*p^unb!$)u0*zv0(-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['shopnotion.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
